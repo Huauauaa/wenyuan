9d2946430b426541f8da4c06570fd2b1b414883b
Harvey <harvey0379@163.com>
2022-10-30 00:14:24 +0800

    feat: polished button colors

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,38 +1,86 @@
 import styled from 'styled-components';
+import { lighten } from 'polished';
+import colors from '../styles/colors';
 
-export type ButtonProps = {
-  type?: string;
+export interface ButtonProps {
+  type: ColorType;
   children?: React.ReactNode;
-};
+}
 
-const StyledButton = styled.button<{ buttonType: string }>`
-  background-color: ${({ buttonType }) => {
-    if (buttonType === 'primary') {
-      return '#b8f1ed';
+export type ColorType =
+  | 'primary'
+  | 'success'
+  | 'danger'
+  | 'info'
+  | 'default'
+  | 'warning'
+  | 'error';
+
+const StyledButton = styled.button<{ colorType: ColorType }>`
+  background-color: ${({ colorType }) => {
+    if (colorType === 'primary') {
+      return colors.primary.base;
     }
-    if (buttonType === 'success') {
-      return '#b8f1cc';
+    if (colorType === 'success') {
+      return colors.success.base;
     }
-    if (buttonType === 'error') {
-      return '#e26538';
+    if (colorType === 'danger' || colorType === 'error') {
+      return colors.danger.base;
     }
-    if (buttonType === 'warn') {
-      return '#f3d751';
+    if (colorType === 'warning') {
+      return colors.warning.base;
     }
-    return '#fff';
+    if (colorType === 'info') {
+      return colors.info.base;
+    }
+    return colors.white;
   }};
-  color: ${({ buttonType }) => {
-    if (buttonType === 'primary') {
-      return '#fff';
+  color: ${({ colorType }) => {
+    if (colorType === 'default') {
+      return colors.black;
     }
-    return '#000';
+    return colors.white;
   }};
-  border-radius: 2px;
+  border-color: ${({ colorType }) => {
+    if (colorType === 'primary') {
+      return colors.primary.base;
+    }
+    if (colorType === 'success') {
+      return colors.success.base;
+    }
+    if (colorType === 'danger' || colorType === 'error') {
+      return colors.danger.base;
+    }
+    if (colorType === 'warning') {
+      return colors.warning.base;
+    }
+    if (colorType === 'info') {
+      return colors.info.base;
+    }
+    return '#dcdfe6';
+  }};
+  border-radius: 4px;
+  outline: none;
+
+  :hover {
+    opacity: 0.6;
+    border-color: ${({ colorType }) => {
+      if (colorType === 'default') {
+        return lighten(0.2, colors.primary.base);
+      }
+      return 'transparent';
+    }};
+    background-color: ${({ colorType }) => {
+      if (colorType === 'default') {
+        return lighten(0.3, colors.primary.base);
+      }
+      return '';
+    }};
+  }
 `;
 
-const Button = (props: ButtonProps = {}) => {
-  const { type = 'default', children } = props;
-  return <StyledButton buttonType={type}>{children}</StyledButton>;
+const Button = ({ children, type }: ButtonProps = { type: 'default' }) => {
+  return <StyledButton colorType={type}>{children}</StyledButton>;
 };
 
 export default Button;
